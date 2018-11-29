94e52d64bfc1b4241c7d4493a300136d7d488e28
MasahiroYoshida <chrbro15@gmail.com>
2018-11-29 01:31:09 -0600

    add connection between renderer and server

--- renderer.py
+++ renderer.py
@@ -2,23 +2,30 @@ import socket, optparse
 import threading
 from os import fork
 
-def handle_controller_connection(controller_socket):
-    logger = open('log_ser.txt', 'a')
+def handle_controller_connection(controller_socket, RtoS_socket):
+    logger = open('log_ren.txt', 'a')
     request = controller_socket.recv(1024)
     logger.write('Received %s\n' % (request))
     controller_socket.send('ACK-renderer')
+    RtoC_socket.send(request)
+    response = RtoC_socket.recv(4096)
+    logger.write(response)
+    logger.flush()
     logger.close()
     controller_socket.close()
 
-def handle_controller(RtoC_socket):
+def handle_controller(RtoC_socket, RtoS_socket):
     while True:
         controller_sock, address = RtoC_socket.accept()
         print 'Accepted connection from {}:{}'.format(address[0], address[1])
         client_handler = threading.Thread(
             target=handle_controller_connection,
-            args=(controller_sock,)
+            args=(controller_sock,RtoS_socket,)
         )
         client_handler.start()
+        
+
+
 
 def main():
     parser = optparse.OptionParser()
@@ -30,13 +37,22 @@ def main():
     port_RtoS_command = 50002
     port_RtoS_contents = 50003
 
-    bind_ip = options.ipr
+    bind_ip_ren = options.ipr
 
     RtoC_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    RtoC_socket.bind((bind_ip, port_RtoC))
+    RtoC_socket.bind((bind_ip_ren, port_RtoC))
     RtoC_socket.listen(5)  # max backlog of connections
 
-    handle_controller(RtoC_socket)
+    RtoS_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    RtoC_socket.connect((options.ips, port_RtoS_command))
+    
 
+
+    #pid = fork()
+    #if(pid == 0):
+    #    handle_command(RtoS_socket)
+    #elif(pid > 0):
+    #    handle_controller(RtoC_socket)
+    handle_controller(RtoC_socket, RtoS_socket)
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def message_files():
     return message_serialized
 
 def handle_controller_connection(controller_socket):
-    logger = open('log_ser.txt', 'a')
+    logger = open('log_ser_con.txt', 'a')
     request = controller_socket.recv(1024)
     logger.write('Received %s\n' % (request))
     message = message_files()
@@ -32,6 +32,23 @@ def handle_controller(StoC_socket):
         )
         client_handler.start()
 
+def handle_renderer_connection(renderer_socket):
+    logger = open('log_ser_ren.txt', 'a')
+    request = renderer_socket.recv(1024)
+    logger.write('Received %s\n' % (request))
+    renderer_socket.send('ACK -server')
+    logger.close()
+    renderer_socket.close()
+
+def handle_renderer(RtoS_socket):
+    while True:
+        renderer_sock, address = RtoS_socket.accept()
+        print 'Accepted connection from {}:{}'.format(address[0], address[1])
+        client_handler = threading.Thread(
+            target=handle_renderer_connection,
+            args=(renderer_sock,)
+        )
+        client_handler.start()
 
 def main():
     parser = optparse.OptionParser()
@@ -40,15 +57,20 @@ def main():
     (options, args) = parser.parse_args()
     bind_ip = options.ips
     port_CtoS = 50000
+    port_RtoS_command = 50002
+
     StoC_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     StoC_socket.bind((bind_ip, port_CtoS))
     StoC_socket.listen(5)  # max backlog of connections
 
+    RtoS_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    RtoS_socket.bind((bind_ip, port_RtoS_command))
+    RtoS_socket.listen(5)  # max backlog of connections
+
     pid = fork()
 
     if(pid == 0):
-        #do something
-        sleep(3)
+        handle_renderer(RtoS_socket)
     elif(pid > 0):
         handle_controller(StoC_socket)
 
