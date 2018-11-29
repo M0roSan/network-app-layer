6be7e8b744e8543557eb0ed115e7274358c17387
MasahiroYoshida <chrbro15@gmail.com>
2018-11-29 00:10:30 -0600

    message json

--- controller.py
+++ controller.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python
+import sys
 import socket, optparse
+import json
+
+def message_request(filename, command, contents=None):
+    """Create message: JSON like object"""
+    message = {'filename': filename, 'request': command, 'contents': contents}
+    message_serialized = json.dumps(message)
+    return message_serialized
 
 def main():
     parser = optparse.OptionParser()
     parser.add_option('--is', dest='ips', default='127.0.0.1')
     parser.add_option('--ir', dest='ipr', default='127.0.0.1')
     parser.add_option('-c', dest='command', type='int', default='0')
+    parser.add_option('-f', dest='filename', default='sample.txt')
     (options, args) = parser.parse_args()
 
     port_CtoS = 50000
-    CtoS_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    CtoS_socket.connect((options.ips, port_CtoS))
-    logger = open('log_con.txt', 'w')
+    
+    command = options.command
+    filename = options.filename
 
-    CtoS_socket.send('GET REQUEST')
-    response = CtoS_socket.recv(4096)
-    logger.write(response)
-    logger.flush()
-    logger.close()
+    if not(command == 1 or command == 2 or command == 3 or command == 4 or command == 5):
+        print('ERROR REQUEST TYPE. PLEASE TYPE NUMBER FROM 1 to 5. \n e.g.) python controller.py -c 3\n')
+        sys.exit()
+    else:
+        CtoS_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        CtoS_socket.connect((options.ips, port_CtoS))
+        logger = open('log_con.txt', 'w')
+        message = message_request(filename, command)
+        CtoS_socket.send(message)
+        response = CtoS_socket.recv(4096)
+        logger.write(response)
+        logger.flush()
+        logger.close()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import socket, optparse
 import threading
-from os import listdir
+from os import listdir, fork
 
 import json
 
@@ -21,6 +21,16 @@ def handle_controller_connection(controller_socket):
     logger.close()
     controller_socket.close()
 
+def handle_controller(StoC_socket):
+    while True:
+        controller_sock, address = StoC_socket.accept()
+        print 'Accepted connection from {}:{}'.format(address[0], address[1])
+        client_handler = threading.Thread(
+            target=handle_controller_connection,
+            args=(controller_sock,)
+        )
+        client_handler.start()
+
 def main():
     parser = optparse.OptionParser()
     parser.add_option('--ir', dest='ipr', default='')
@@ -32,14 +42,12 @@ def main():
     StoC_socket.bind((bind_ip, port_CtoS))
     StoC_socket.listen(5)  # max backlog of connections
 
-    while True:
-        controller_sock, address = StoC_socket.accept()
-        print 'Accepted connection from {}:{}'.format(address[0], address[1])
-        client_handler = threading.Thread(
-            target=handle_controller_connection,
-            args=(controller_sock,)
-        )
-        client_handler.start()
+    pid = fork()
+
+    if pid == 0:
+        #do something
+    elif pid > 0:
+        handle_controller(StoC_socket)
 
 if __name__ == '__main__':
     main()
